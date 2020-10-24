6d48dafc1df65d8061fe03b337e2898de0079198
kcroo <kirstencorrao@gmail.com>
2020-10-24 10:32:28 -0700

    more test cases

--- src/utils/calculateClosestPoint.test.ts
+++ src/utils/calculateClosestPoint.test.ts
@@ -1,29 +1,79 @@
 import { Position, getClosestPoint } from './calculateClosestPoint'
 
-// Eugene
-const origin: Position = {
-    lat: 44.032883,
-    long: -123.090570
-};
+describe('getClosestPoint', () => {
+    const eugene: Position = {lat: 44.032883, long: -123.090570};               // closest: Springfield
+    const medford: Position = {lat: 42.282803, long: -122.771226};              // closest: Grants Pass
+    const steensMountain: Position = {lat: 42.6202388, long: -118.6244009};     // closest: Malheur Lake
+    const burns: Position = {lat: 43.5835747, long: -119.0585662};              // closest: Malheur Lake
+    const malheurLake: Position = {lat: 43.3302915, long: -118.8115772};        // closest: Burns
+    const joseph: Position = {lat: 45.3506504, long: -117.2287770};             // closest: La Grande
+    const laGrande: Position = {lat: 45.3276834, long: -118.0967850};           // closest: Joseph       
+    const tillamook: Position = {lat: 45.4564555, long: -123.8370981};          // closest: Oceanside
+    const oceanside: Position = {lat: 45.4577467, long: -123.9673364};          // closest: Tillamook
+    const springfield: Position = {lat: 44.0523465, long: -122.9966305};        // closest: Eugene
+    const grantsPass: Position = {lat: 42.4382946, long: -123.3212346};         // closest: Medford
+    
+    const locations: Position[] = [
+        eugene, medford, steensMountain, burns, malheurLake, joseph, laGrande, tillamook, oceanside, springfield, grantsPass
+    ]
 
-const closest: Position = {lat: 44.159490, long: -123.572924};      // Coast Mountains W of Eugene
-const second: Position = {lat: 42.282803, long: -122.771226};       // Medford
-const third: Position = {lat: 45.534046, long: -118.849208};        // S of Pendleton
+    test("returns Springfield as Eugene's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== eugene);
+        const closest = getClosestPoint(eugene, locationsFilter);
+        expect(closest).toBe(springfield);
+    });
 
-test('finds closest location: index 0', () => {
-    const locations: Position[] = [closest, second, third];
-    const result = getClosestPoint(origin, locations);
-    expect(result).toBe(closest);
-});
+    test("returns Grants Pass as Medford's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== medford);
+        const closest = getClosestPoint(medford, locationsFilter);
+        expect(closest).toBe(grantsPass);
+    });
 
-test('finds closest location: index 1', () => {
-    const locations: Position[] = [second, closest, third];
-    const result = getClosestPoint(origin, locations);
-    expect(result).toBe(closest);
-});
+    test("returns Malheur Lake as Steens's Mountain's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== steensMountain);
+        const closest = getClosestPoint(steensMountain, locationsFilter);
+        expect(closest).toBe(malheurLake);
+    });
 
-test('finds closest location: index 2', () => {
-    const locations: Position[] = [second, third, closest];
-    const result = getClosestPoint(origin, locations);
-    expect(result).toBe(closest);
+    test("returns Malheur Lake as Burns' closest point", () => {
+        const locationsFilter = locations.filter( location => location !== burns);
+        const closest = getClosestPoint(burns, locationsFilter);
+        expect(closest).toBe(malheurLake);
+    });
+
+    test("returns La Grande as Joseph's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== joseph);
+        const closest = getClosestPoint(joseph, locationsFilter);
+        expect(closest).toBe(laGrande);
+    });
+
+    test("returns Joseph as La Grande's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== laGrande);
+        const closest = getClosestPoint(laGrande, locationsFilter);
+        expect(closest).toBe(joseph);
+    });
+
+    test("returns Oceanside as Tillamook's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== tillamook);
+        const closest = getClosestPoint(tillamook, locationsFilter);
+        expect(closest).toBe(oceanside);
+    });
+
+    test("returns Tillamook as Oceanside's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== oceanside);
+        const closest = getClosestPoint(oceanside, locationsFilter);
+        expect(closest).toBe(tillamook);
+    });
+
+    test("returns Eugene as Springfield's closest point", () => {
+        const locationsFilter = locations.filter( location => location !== springfield);
+        const closest = getClosestPoint(springfield, locationsFilter);
+        expect(closest).toBe(eugene);
+    });
+
+    test("returns Medford as Grants Pass' closest point", () => {
+        const locationsFilter = locations.filter( location => location !== grantsPass);
+        const closest = getClosestPoint(grantsPass, locationsFilter);
+        expect(closest).toBe(medford);
+    });
 });
