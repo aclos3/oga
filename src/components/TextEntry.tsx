63928f285cb3736bdedbabf3b9f00b09c5a99f2e
aclos3 <45724446+aclos3@users.noreply.github.com>
2020-10-24 19:36:41 -0700

    Made fixes to state strings
    OR as a state should work now.

--- src/components/TextEntry.tsx
+++ src/components/TextEntry.tsx
@@ -3,6 +3,7 @@ import './TextEntry.css';
 import { observable } from "mobx"
 import { IonInput, IonLabel, IonItem, IonButton, IonLoading, IonToast } from '@ionic/react';
 import { Controller, useForm } from 'react-hook-form';
+import { setTextRange } from 'typescript';
 
 interface ContainerProps {}
 interface DataError {
@@ -46,6 +47,7 @@ const TextEntry: React.FC<ContainerProps> = () => {
         .then(response => response.json())
         .then(data => {
             myData = data;
+            console.log(`api str: `, state.textEntry)
             if(myData.records[0] === undefined) { alert(`No results found for your entry. Please check for typos.`)}
             else {
                 if(myData.records[0].fields.geopoint[0] && myData.records[0].fields.geopoint[1]) {
@@ -63,23 +65,36 @@ const TextEntry: React.FC<ContainerProps> = () => {
     }
 
     const getValid = (data: any) => {
-
         state.setText(data.text)
         let regExp = /^[\w ]+,[ ]?[A-Za-z]{2}$/ //regex to check if format is comma separated city state pair
         
+        //find comma
+        let idx = 0
+        for(let i = 0; i < state.textEntry.length; i++) {
+            if(state.textEntry.charAt(i) === ',') { idx = i }
+        }
+        //remove spaces after comma
+        for(let i = idx; i < state.textEntry.length; i++) {
+            if(state.textEntry.charAt(i) === ' ') {
+                state.setText(state.textEntry.substring(0, i) + state.textEntry.substring(i + 1))
+                i--
+            }
+        }
+        //console.log("replaced spaces after commas: ", state.textEntry, `text len: `, state.textEntry.length, `idx: `, idx)
         //determine if the entry is a city/state pair
         if(regExp.test(state.textEntry)) {
-            console.log(`City state syntax valid!`)
-             //add space after comma
-             state.setText(state.textEntry.replace(/,/g, ', '))
-             getData()
+            //console.log(`City state syntax valid!`)
+            state.setText(state.textEntry.replace(/,/g, ',+\''))
+            //console.log("replaced spaces after commas: ", state.textEntry)
+            getData()
         }
         //determine if entry is a valid zip code
         else if(!(isNaN(state.textEntry)) && state.textEntry.length === 5) {
-            console.log(`zip valid`)
+            //console.log(`zip valid`)
             getData()
         }
-        else {console.log(`invalid`)}
+        //check for more than two characters after comma
+        else {alert(`Entry is invalid, please try again. You must use the two letter postal abbreviation for the state.`)}
     }
     return (
         <div className="zipContainer">
