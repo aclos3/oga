85ede398a83cf509d74cf4babe2e424dfb663714
lebensjohns <45158773+lebensjohns@users.noreply.github.com>
2020-11-23 11:10:00 -0800

    About page now has inner links

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,4 +1,4 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonBackButton, IonButtons, IonIcon, IonContent, IonText } from '@ionic/react'
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonBackButton, IonButtons, IonIcon, IonContent, IonText, IonList, IonItem } from '@ionic/react'
 import { helpCircle} from 'ionicons/icons';
 import { RouteComponentProps } from 'react-router';
 import React from 'react';
@@ -7,6 +7,7 @@ import './InfoPage.css';
 
 interface ContainerProps extends RouteComponentProps<{
 }> {}
+
 
 const InfoPage: React.FC<ContainerProps> = ({ match, history }) => { 
     return (
@@ -20,15 +21,33 @@ const InfoPage: React.FC<ContainerProps> = ({ match, history }) => {
             <IonTitle className="results-title">Frost Date Finder</IonTitle>
           </div>
         </IonToolbar>
+      </IonHeader>
         <IonContent>
           <IonText>
-            <h1 className='about-title'>
-              About The App
-            </h1>
-            <p>This app was designed and created by a group of </p>
+            <h1 className='about-title'> About Page</h1>
           </IonText>
+          <IonList>
+            <IonItem><a href="/dashboard/info#frost-data"> Frost Data </a></IonItem>
+            <IonItem><a href="/dashboard/info#location-data"> Location Data </a></IonItem>
+            <IonItem><a href="/dashboard/info#the-team"> The Team </a></IonItem>
+            <IonItem><a href="/dashboard/info#our-partners"> Our Partners </a></IonItem>
+            <div id="frost-data">
+              <h3>Frost Data</h3>
+                <p>All of the weather data presented on this app came directly 
+                  from the National Oceanic and Atmospheric Administration (NOAA). </p>
+            </div>
+            <div id="location-data">
+              <h3>Location Data</h3>
+            </div>
+            <div id="the-team">
+              <h3>The Team</h3>
+            </div>
+            <div id="our-partners">
+              <h3>Our Partners</h3>
+            </div>
+          </IonList>
         </IonContent>
-      </IonHeader>
+      
     </IonPage>
   );
 };
