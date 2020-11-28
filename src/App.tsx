90dc2266a7880eb35acef0ccb77d1203c3916bac
kcroo <kirstencorrao@gmail.com>
2020-11-27 20:59:39 -0800

    no white screen between splash and dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,8 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-const { SplashScreen } = Plugins;
 
 const App: React.FC = () => {
-  SplashScreen.hide();
-
   return(
     <IonApp>
       <IonReactRouter>
